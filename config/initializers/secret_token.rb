383e09530b8f94a9482c0b77bc96e5ddf3301127
Doug MacDowell <dougmac9@uw.edu>
2014-01-26 16:52:31 -0800

    First commit of Week 3 minitest exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '5bd359f478b0d90154ef172ec954bc971d6f7fc859dc12e23460f3790abd8c93aed1ca678b4e6aed77f2fdd3e3fb2f1f86dc113ed7b035f5bfd7e03094db17fb'
